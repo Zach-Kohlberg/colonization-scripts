55a921186e0dd398161265c06b762e3472ad991f
Ron M Burgess <ronaldmburgess@sbcglobal.net>
2015-04-14 02:33:55 -0400

    GUI 4-13
    Added some GUI functions. The basic layout/format is now there. Although
    this won't be seen without the corresponding package.
    
    https://drive.google.com/open?id=0B3AY1tb1Jvy7TTFCRDFnNWFwWlE&authuser=0

---
+++ UI/ActionBarButtonsScript.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class ActionBarButtonsScript : MonoBehaviour {
+
+    public Text actionbarHoverText;
+    public string actionBarTextInfo;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void ActionBarMouseIn()
+    {
+        actionbarHoverText.text = actionBarTextInfo;
+    }
+
+    public void ActionBarMouseOut()
+    {
+        actionbarHoverText.text = " ";
+    }
+}
