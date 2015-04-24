71b5d65dceb9455f204756340c0de57f049f6966
Ron M Burgess <ronaldmburgess@sbcglobal.net>
2015-04-23 22:34:16 -0400

    Fog And Beacon
    Added fog from Ryan, but it didn't work.

---
+++ LightObject.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LightObject : MonoBehaviour {
+	bool large = false;
+	SphereCollider lightSphere;
+	Transform lightScale;
+	public float maxBounds, lowerBounds;
+	// Use this for initialization
+	void Start() {
+		lightSphere = GetComponent<SphereCollider> ();
+		lightScale = gameObject.transform;
+
+	}
+
+	void Update() {
+		if (Input.GetKeyDown (KeyCode.F)) 
+			ShrinkLight ();
+		else if (Input.GetKeyDown (KeyCode.G))
+			GrowLight ();
+	}
+
+	public void GrowLight() {
+		if (!large) {
+			for (float i = lowerBounds; i < maxBounds; i++) {
+				//lightSphere.radius += i/2;
+				lightScale.localScale = new Vector3 (lightScale.localScale.x + .01f, lightScale.localScale.y + .01f, lightScale.localScale.z + .01f);
+			}
+			large = true;
+		}
+
+	}
+
+	public void ShrinkLight() {
+		if (large) {
+			for (float i = maxBounds; i > lowerBounds; i--) {
+				//lightSphere.radius -= i/2;
+				lightScale.localScale = new Vector3 (lightScale.localScale.x - .01f, lightScale.localScale.y - .01f, lightScale.localScale.z - .01f);
+			}
+			large = false;
+		}
+	}
+}
